60c70b850a1984dd2a8448b37ed6c7e949ec7a5a
Altin Thaci <altin.thaci@fresha.com>
2024-04-21 17:11:55 +0200

    save, load, create deck, erlang binary, fs

--- bootcamp/cards/lib/cards.ex
+++ bootcamp/cards/lib/cards.ex
@@ -1,17 +1,4 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spaces", "Clubs", "Hearts", "Diamonds"]
@@ -31,5 +18,23 @@ defmodule Cards do
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _} -> "File does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    create_deck()
+    |> shuffle()
+    |> deal(hand_size)
   end
 end
